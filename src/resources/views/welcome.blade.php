fed06db0763e8a9faeece5b6ef444dea7d9d1459
nexus-arai <nexus.takuya.arai@gmail.com>
2022-07-13 11:35:58 +0900

    お問い合わせフォームの配置を横並びにさせました

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -10,49 +10,48 @@
 </head>
 
 <body>
-    <section class="contents">
-        <div class="title-content">
-            <div class="text-center">
-                <p class="title text-center">CONTACT</p>
-                <p class="middle-title text-center">お問い合わせ</p>
-                <p class="explain text-center">物件に関するご質問など、お気軽に問い合わせください。</p>
+    <main class="page">
+        <div class="main-title">
+            <p class="title text-center">CONTACT</p>
+            <p class="middle-title text-center">お問い合わせ</p>
+            <p class="explain text-center">物件に関するご質問など、お気軽に問い合わせください。</p>
+        </div>
+        <div class="Form">
+            <div class="Form-Item">
+                <div class="Form-Item-Label">名前</div>
+                <div class="Form-Item">
+                    <input type="text" class="Form-Item-Input" id="name" placeholder="Suzuki Taro">
+                </div>
             </div>
         </div>
-        <div class="display-flex">
-            <div class="">氏名</p>
-            </div>
-            <div class="">
-                <input type="text" class="" id="name" placeholder="Suzuki Taro">
-            </div>
-        </div>
-
-        <div class="display-flex">
-            <div class="left-item important-item">
-                <p class="">メールアドレス</p>
-            </div>
-            <div class="right-input">
-                <input type="email" class="form-control contact-input" id="email" placeholder="abcd@gmail.com">
+        <div class="Form">
+            <div class="Form-Item">
+                <div class="Form-Item-Label">メールアドレス</div>
+                <div class="Form-Item">
+                    <input type="email" class="Form-Item-Input" id="email" placeholder="abcd@gmail.com">
+                </div>
             </div>
         </div>
-        <div class="display-flex">
-            <div class="left-item">
-                <p>電話番号</p>
-            </div>
-            <div class="right-input">
-                <input type="text" class="form-control contact-input" id="contact" placeholder="+81 123 456 789">
-            </div>
-        </div>
-        <div class="display-flex">
-            <div class="top-item important-item">
-                <p class="">お問い合わせ内容</p>
-            </div>
-            <div class="right-textarea">
-                <textarea class="form-control contact-input" id="details" rows="7" placeholder="物件に関するご質問など、お気軽にお問い合わせください。"></textarea>
+        <div class="Form">
+            <div class="Form-Item">
+                <div class="Form-Item-Label">電話番号</div>
+                <div class="Form-Item">
+                    <input type="text" class="Form-Item-Input" id="tel" placeholder="+81 123 456 789">
+                </div>
             </div>
         </div>
-        <button type="submit" class="btn btn-submit">送信する</button>
+        <div class="Form">
+            <div class="Form-Item">
+                <div class="Form-Item-Label">お問い合わせ内容</div>
+                <div class="Form-Item">
+                    <textarea class="Form-Item-Input" id="details" rows="7" placeholder="物件に関するご質問など、お気軽にお問い合わせください。"></textarea>
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="display-flex btn btn-submit">送信する</button>
+        </div>
         </div>
         <script></script>
-    </section>
+        </section>
 
-    <body>
+        <body>
